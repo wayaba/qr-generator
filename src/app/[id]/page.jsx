d6c99a938771ea14772fca4f4f197d06314aef1c
Pablo Pedraza <wensel84@gmail.com>
2023-08-01 09:14:49 -0300

    alineo qr

--- src/app/[id]/page.jsx
+++ src/app/[id]/page.jsx
@@ -6,7 +6,7 @@ export default function CustomHome({ params }) {
   const { id } = params
 
   return (
-    <section className="z-50 bg-white w-fit rounded-lg p-4 shadow-lg">
+    <section className="z-50 bg-white w-fit rounded-lg p-4 shadow-lg m-auto">
       <QRCode urlParam={decodeURIComponent(id)} isCustom={true} />
     </section>
   )
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,7 +3,7 @@ import { QRCode } from '@/components/QrCode'
 
 export default function Home() {
   return (    
-      <section className="z-50 bg-white w-fit rounded-lg p-4 shadow-lg">
+      <section className="z-50 bg-white w-fit rounded-lg p-4 shadow-lg m-auto">
         <QRCode/>
       </section>
   )
